ec18c74b6078e1a81084fb949b72371d26cd0bbb
Delfim Marime <Delfimarime@gmail.com>
2025-03-10 20:49:09 +0200

    wip:mixing

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -48,7 +48,7 @@ export function NavigationBar() {
                         </button>
                         {userMenuOpen && (
                             <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg p-2">
-                                <Link to="/logout" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
+                                <Link to="/" className="block px-4 py-2 hover:bg-gray-100">Logout</Link>
                             </div>
                         )}
                     </div>

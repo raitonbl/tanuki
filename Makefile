65733c92e906b2e43331a31c0cd74635816d9192
Delfim Marime <Delfimarime@gmail.com>
2025-03-16 16:12:19 +0200

    wip:mixing

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 run:
 	cd backend && go run cmd/main.go serve
+
+run-tls:
+	export TANUKI_SERVER_SERVERS_REGISTRY_TLS_KEY=key.pem && \
+	 export TANUKI_SERVER_SERVERS_REGISTRY_TLS_CERT=cert.pem && \
+	 cd backend && go run cmd/main.go serve
--- backend/internal/config/spf13.go
+++ backend/internal/config/spf13.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"github.com/thoas/go-funk"
 	"os"
+	"strings"
 )
 
 const (
@@ -24,92 +24,120 @@ type Flags interface {
 }
 
 func NewConfigurationFromFlags(flags Flags) (Config, error) {
-	viperInstance := viper.New()
-	viperInstance.SetEnvPrefix(EnvironmentVariablePrefix)
-	viperInstance.AutomaticEnv()
-
-	_ = viperInstance.BindPFlag("target", flags.Lookup("target"))
-	_ = viperInstance.BindPFlag("database.username", flags.Lookup("database.username"))
-	_ = viperInstance.BindPFlag("database.password", flags.Lookup("database.password"))
-	_ = viperInstance.BindPFlag("database.name", flags.Lookup("database.name"))
-	_ = viperInstance.BindPFlag("database.host", flags.Lookup("database.host"))
-	_ = viperInstance.BindPFlag("database.port", flags.Lookup("database.port"))
-	_ = viperInstance.BindPFlag("database.type", flags.Lookup("database.type"))
-	_ = viperInstance.BindPFlag("database.options", flags.Lookup("database.options"))
-	_ = viperInstance.BindPFlag("servers.registry.port", flags.Lookup("servers.registry.port"))
-	_ = viperInstance.BindPFlag("servers.management.port", flags.Lookup("servers.management.port"))
-	_ = viperInstance.BindPFlag("log-level", flags.Lookup("log-level"))
-
-	configFilePath := os.Getenv(ConfigurationFileEnvironmentVariable)
-	isConfigFileUserDefined := configFilePath != ""
-	if !isConfigFileUserDefined {
-		configFilePath = "local.yaml"
-	}
-	_, err := os.Stat(configFilePath)
+	viperInstance := createViperInstance()
+	bindFlagsToViper(viperInstance, flags)
+	err := readViperConfigurationFile(viperInstance)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return Config{}, err
-		}
-		if isConfigFileUserDefined {
-			return Config{}, err
-		}
-	} else {
-		viperInstance.SetConfigFile(configFilePath)
-		if err = viperInstance.ReadInConfig(); err != nil {
-			return Config{}, err
-		}
+		return Config{}, err
 	}
+	fmt.Println(
+		"-------------------",
+	)
+	for k, v := range viperInstance.AllKeys() {
+		fmt.Println(k, v)
+	}
+	fmt.Println(
+		"-------------------",
+	)
 	var instance Config
 	if err = viperInstance.Unmarshal(&instance); err != nil {
 		return Config{}, err
 	}
-	if instance.LogLevel == "" {
-		instance.LogLevel = InfoLogLevel
-	}
-	if !funk.Contains([]string{DebugLogLevel, InfoLogLevel}, instance.LogLevel) {
-		return instance, fmt.Errorf("log-level must be either %s or %s", InfoLogLevel, DebugLogLevel)
-	}
-	if instance.Environment == "" {
-		instance.Environment = DefaultEnvironment
-	}
-	if instance.Service == "" {
-		instance.Service = DefaultServiceName
-	}
-	if instance.Targets == nil {
-		instance.Targets = getDefaultTargets()
-	}
-	if instance.Servers.Registry.Port == nil {
-		instance.Servers.Registry.Port = funk.PtrOf(DefaultServerPort).(*int)
-	}
 
-	assertTLSConfigurationOnServer := func(target Server, name string) error {
-		if target.TLS == nil {
-			return nil
-		}
-		if target.TLS.CertFile == "" {
-			return fmt.Errorf("servers.%s.tls.cert must be set", name)
-		}
-		if _, prob := os.Stat(target.TLS.CertFile); prob != nil {
-			return fmt.Errorf("servers.%s.tls.cert=%s couldn't be accessed due to: %v", name, target.TLS.CertFile, prob)
-		}
-		if target.TLS.KeyFile == "" {
-			return fmt.Errorf("servers.%s.tls.key must be set", name)
-		}
-		if _, prob := os.Stat(target.TLS.KeyFile); prob != nil {
-			return fmt.Errorf("servers.%s.tls.key=%s couldn't be accessed due to: %v", name, target.TLS.KeyFile, prob)
-		}
-		return nil
-	}
+	setConfigurationDefaults(&instance)
 
-	if err = assertTLSConfigurationOnServer(instance.Servers.Registry, "registry"); err != nil {
+	if err = validateServersTLSConfiguration(instance.Servers.Registry, "registry"); err != nil {
 		return Config{}, err
 	}
-
-	if err = assertTLSConfigurationOnServer(instance.Servers.Management, "management"); err != nil {
+	if err = validateServersTLSConfiguration(instance.Servers.Management, "management"); err != nil {
 		return Config{}, err
 	}
 
 	return instance, nil
+}
+
+func createViperInstance() *viper.Viper {
+	v := viper.New()
+	v.SetEnvPrefix(EnvironmentVariablePrefix)
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+	return v
+}
+
+func bindFlagsToViper(v *viper.Viper, flags Flags) {
+	flagMappings := []string{
+		"target",
+		"database.username", "database.password", "database.name", "database.host", "database.port", "database.type", "database.options",
+		"servers.registry.port", "servers.registry.tls.key", "servers.registry.tls.cert",
+		"servers.management.port", "servers.management.tls.key", "servers.management.tls.cert",
+		"log-level",
+	}
+
+	for _, key := range flagMappings {
+		_ = v.BindPFlag(key, flags.Lookup(key))
+	}
+}
+
+func readViperConfigurationFile(v *viper.Viper) error {
+	configFilePath := os.Getenv(ConfigurationFileEnvironmentVariable)
+	isUserDefined := configFilePath != ""
+	if configFilePath == "" {
+		configFilePath = "local.yaml"
+	}
+	if _, err := os.Stat(configFilePath); err != nil {
+		if os.IsNotExist(err) && !isUserDefined {
+			return nil // No config file is fine unless explicitly specified
+		}
+		return err
+	}
+	v.SetConfigFile(configFilePath)
+	if err := v.ReadInConfig(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setConfigurationDefaults(config *Config) {
+	if config.LogLevel == "" {
+		config.LogLevel = InfoLogLevel
+	}
+	if config.Environment == "" {
+		config.Environment = DefaultEnvironment
+	}
+	if config.Service == "" {
+		config.Service = DefaultServiceName
+	}
+	if config.Targets == nil {
+		config.Targets = getDefaultTargets()
+	}
+	if config.Servers.Registry.Port == nil {
+		defaultPort := DefaultServerPort
+		config.Servers.Registry.Port = &defaultPort
+	}
+
+	validLogLevels := map[string]bool{DebugLogLevel: true, InfoLogLevel: true}
+	if !validLogLevels[config.LogLevel] {
+		panic(fmt.Errorf("log-level must be either %s or %s", InfoLogLevel, DebugLogLevel))
+	}
+}
+
+func validateServersTLSConfiguration(server Server, name string) error {
+	if server.TLS == nil {
+		return nil
+	}
+	requiredFiles := map[string]string{
+		"cert": server.TLS.CertFile,
+		"key":  server.TLS.KeyFile,
+	}
+	for field, path := range requiredFiles {
+		if path == "" {
+			return fmt.Errorf("servers.%s.tls.%s must be set", name, field)
+		}
+		if _, err := os.Stat(path); err != nil {
+			return fmt.Errorf("servers.%s.tls.%s=%s couldn't be accessed: %v", name, field, path, err)
+		}
+	}
+	return nil
 }
 
 func getDefaultTargets() []string {
